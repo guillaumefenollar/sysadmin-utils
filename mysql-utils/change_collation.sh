949d04ecad67ebb2434626a2102dfb05580482e1
Guillaume Fenollar <guillaume.fenollar@gmail.com>
2012-06-16 14:24:46 +0200

    Adding change_collation.sh which helps to convert xwiki DB into mysql to the right collation (mysql_bin) which is insensitive (instead of general_ci. ci means case insensitive).

---
+++ mysql-utils/change_collation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mysql_cmd=mysql -u root -p
+
+TBL_LIST=$($mysql_cmd -N -s -r -e 'use xwiki;show tables;')
+
+for tbl_name in $TBL_LIST;
+do
+$mysql_cmd -e "alter table xwiki.$tbl_name convert to character set utf8 collate utf8_general_ci;"
+done
+
+echo "Here the result of the operation:"
+$mysql_cmd -e 'SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLLATION_NAME     FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA="xwiki";'
