9616192a7b935addfeee42264b2a568933919223
Guillaume Fenollar <guillaume.fenollar@gmail.com>
2012-06-22 19:00:44 +0200

    Adding detect_collation script which help to detect collation of a column of a wqtable of each DB which is not using utf8_bin

---
+++ mysql-utils/detect_collation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MYSQL_COMMAND=mysql
+
+DATABASES=$(mysql -N -s -r -e 'show databases'|grep -v ^information_schema$|grep -v ^mysql$)
+
+for db in $DATABASES
+do
+
+$MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collation FROM information_schema.tables WHERE table_schema = DATABASE() AND table_collation <> 'utf8_bin'"
+
+done
