b6bc6afa9f73f57189064d8fa81c5f435730cb40
Guillaume Fenollar <guillaume.fenollar@xwiki.com>
2012-06-25 10:42:51 +0300

    Adding some variables and changing the select command

--- mysql-utils/change_collation.sh
+++ mysql-utils/change_collation.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-mysql_cmd=mysql -u root -p
+db=sugarcrm
+to_character_set=utf8
+to_collation=utf8_bin
 
-TBL_LIST=$($mysql_cmd -N -s -r -e 'use xwiki;show tables;')
+mysql_cmd="mysql -u root"
+
+######
+
+
+TBL_LIST=$($mysql_cmd -N -s -r -e "use $db;show tables;")
 
 for tbl_name in $TBL_LIST;
 do
-$mysql_cmd -e "alter table xwiki.$tbl_name convert to character set utf8 collate utf8_bin;"
+$mysql_cmd -e "alter table $db.$tbl_name convert to character set $to_character_set collate to_collation;"
 done
 
 echo "Here the result of the operation:"
-$mysql_cmd -e 'SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLLATION_NAME     FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA="xwiki";'
+$mysql_cmd -e "USE $db;SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLLATION_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA=DATABASE();"
