4ee549cf7d4b149309ee2b6fea3d6d881e409656
Guillaume Fenollar <guillaume.fenollar@gmail.com>
2012-06-26 16:59:27 +0200

    Minor changes

--- mysql-utils/detect_spam.sh
+++ mysql-utils/detect_spam.sh
@@ -3,19 +3,22 @@
 ## Built to work a on XWiki Schema (3.x or 4.x)
 nbcoms=50  ## Number of comments above which you get alerted (Spam Threshold).
 
-DATABASES=$(mysql -N -s -r -e 'show databases'|grep -v ^information_schema$|grep -v ^mysql$) ## You can exclude DBs from the list here (adding a grep -v)
+MYSQL_COMMAND="mysql"
+DATABASES=$($MYSQL_COMMAND -N -s -r -e 'show databases'|grep -v ^information_schema$|grep -v ^mysql$) ## You can exclude DBs from the list here (adding a grep -v)
 
 for db in $DATABASES
 do
 
-CHECK_COMMANDS=$(mysql -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;")
+CHECK_COMMANDS=$($MYSQL_COMMAND -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;")
 
         if [[ $CHECK_COMMANDS != "" ]]
         then
                 echo "We have found more than $nbcoms comments in database  *** $db *** :"
-                mysql -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;"
-                echo ""
+                $MYSQL_COMMAND -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;"
+        else
+                echo "OK"
         fi
+                echo ""
 
 done
 
@@ -46,8 +49,10 @@ DATABASES=$($MYSQL_COMMAND -N -s -r -e 'show databases'|grep -v ^information_sch
 	        then
         	        echo "We have found more than $nbcoms comments in database  *** $db *** :"
                 	$MYSQL_COMMAND -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;"
-	                echo ""
+		else
+			echo "OK"
         	fi
+	                echo ""
 	done
 
 done
