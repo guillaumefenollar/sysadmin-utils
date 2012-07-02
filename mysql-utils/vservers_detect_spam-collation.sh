73984fb9c18774259ba9ac6a669987a3e7f3bc46
Guillaume Fenollar <guillaume.fenollar@gmail.com>
2012-07-02 15:45:01 +0200

    Adding a global script to check collation and spam

---
+++ mysql-utils/vservers_detect_spam-collation.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# This script detect both wrong collation and spam in each table of each database of each running vserver. It finally sends a mail with a global result of the scan
+#
+#### Variables #
+
+mail_add=guillaume.fenollar@xwiki.com   ## To whom send the mail
+nbcoms=50              ## Threshold for spam detection (docs with more comments will be flagged as possibly spammed)
+
+#######
+
+echo "" > $0.mail
+
+for ctx in $(ls /proc/virtual/|grep -v info|grep -v status)
+do
+vm=$(grep $ctx /etc/vservers/*/context|cut -d'/' -f 4)
+
+        echo "Checking VM $vm collation..." >> $0.mail
+        MYSQL_COMMAND="vserver $vm exec mysql"
+        DATABASES=$($MYSQL_COMMAND -N -s -r -e 'show databases'|grep -v ^information_schema$|grep -v ^mysql$)
+
+        for db in $DATABASES
+        do
+
+                RESULT=$($MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collation FROM information_schema.tables WHERE table_schema = DATABASE() AND table_collation <> 'utf8_bin'")
+        done
+                if [[ $RESULT == "" ]]
+                then
+                echo "...OK!" >> $0.mail
+                else
+                $MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collation FROM information_schema.tables WHERE table_schema = DATABASE() AND table_collation <> 'utf8_bin'" >> $0.mail
+                fi
+
+
+        echo "Checking VM $vm for spam..." >> $0.mail
+        for db in $DATABASES
+        do
+        CHECK_COMMANDS=$($MYSQL_COMMAND -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;")
+
+                if [[ $CHECK_COMMANDS != "" ]]
+                then
+                        echo "We have found more than $nbcoms comments in database  *** $db *** :" >> $0.mail
+                        $MYSQL_COMMAND -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;" >> $0.mail
+                        echo "" >> $0.mail
+                else
+                        echo "...OK!" >> $0.mail
+                fi
+        done
+        echo "" >> $0.mail
+
+done
+
+cat $0.mail | mail -s "Spam/collation report for $(hostname) machine" $mail_add
