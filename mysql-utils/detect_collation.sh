0602d3e985d18105cd04096f095ae91f1770a484
Guillaume Fenollar <guillaume.fenollar@gmail.com>
2012-07-02 16:17:38 +0200

    Adding "OK" message when everything is fine on the DB for spam/collation. Adding mail part

--- mysql-utils/detect_collation.sh
+++ mysql-utils/detect_collation.sh
@@ -10,26 +10,3 @@ $MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collatio
 done
 
 
-
-
-################
-###############   - VSERVER UNDER  -    #########
-################
-
-
-
-
-#!/bin/bash
-
-for vm in $(ls -1 /etc/vservers/|grep -v newvserver|grep -v ip.list)
-do
-        echo "Checking VM $vm...     ########################################################################### "
-        MYSQL_COMMAND="vserver $vm exec mysql"
-        DATABASES=$($MYSQL_COMMAND -N -s -r -e 'show databases'|grep -v ^information_schema$|grep -v ^mysql$)
-        for db in $DATABASES
-        do
-          $MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collation FROM information_schema.tables WHERE table_schema = DATABASE() AND table_collation <> 'utf8_bin'"
-        done
-        echo ""
-done
-
--- mysql-utils/vservers_detect_spam-collation.sh
+++ mysql-utils/vservers_detect_spam-collation.sh
@@ -18,21 +18,26 @@ vm=$(grep $ctx /etc/vservers/*/context|cut -d'/' -f 4)
         echo "Checking VM $vm collation..." >> $0.mail
         MYSQL_COMMAND="vserver $vm exec mysql"
         DATABASES=$($MYSQL_COMMAND -N -s -r -e 'show databases'|grep -v ^information_schema$|grep -v ^mysql$)
-
+	
+	ERRCODE=0
         for db in $DATABASES
         do
 
                 RESULT=$($MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collation FROM information_schema.tables WHERE table_schema = DATABASE() AND table_collation <> 'utf8_bin'")
-        done
-                if [[ $RESULT == "" ]]
+                if [[ $RESULT != "" ]]
                 then
-                echo "...OK!" >> $0.mail
-                else
-                $MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collation FROM information_schema.tables WHERE table_schema = DATABASE() AND table_collation <> 'utf8_bin'" >> $0.mail
+                	$MYSQL_COMMAND -e "use $db; SELECT table_schema,table_name,Engine,table_collation FROM information_schema.tables WHERE table_schema = DATABASE() AND table_collation <> 'utf8_bin'" >> $0.mail
+		ERRCODE=1
                 fi
+        done
+		if [[ $ERRCODE == 0 ]]
+		then
+			echo "...OK!" >> $0.mail
+		fi
 
 
         echo "Checking VM $vm for spam..." >> $0.mail
+	ERRCODE=0
         for db in $DATABASES
         do
         CHECK_COMMANDS=$($MYSQL_COMMAND -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;")
@@ -41,13 +46,21 @@ vm=$(grep $ctx /etc/vservers/*/context|cut -d'/' -f 4)
                 then
                         echo "We have found more than $nbcoms comments in database  *** $db *** :" >> $0.mail
                         $MYSQL_COMMAND -e "use $db; select count(XWO_NAME) as coms,XWO_NAME as doc from xwikiobjects where XWO_CLASSNAME='XWiki.XWikiComments' GROUP BY XWO_NAME HAVING coms >= $nbcoms;" >> $0.mail
+			ERRCODE=1
                         echo "" >> $0.mail
-                else
-                        echo "...OK!" >> $0.mail
                 fi
         done
+                if [[ $ERRCODE == 0 ]]
+                then
+                        echo "...OK!" >> $0.mail
+                fi
         echo "" >> $0.mail
 
 done
 
 cat $0.mail | mail -s "Spam/collation report for $(hostname) machine" $mail_add
+
+if [[ $? == 0 ]]
+then
+	rm $0.mail
+fi
