2e8c19457821cb3a39c94a9bccbc2a2bb8747c9b
Guillaume Fenollar <guillaume.fenollar@xwiki.com>
2012-10-01 11:23:07 +0200

    Set rights to vservers_detect_spam_collation, and changed a bit the presentation of detect_size.sh script

--- mysql-utils/detect_size_DB.sh
+++ mysql-utils/detect_size_DB.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+### Variables - set password here after adding -p parameter ###
 mysql_cmd='mysql -u root'
+###
 
 echo "Here the size of databases, where size is above 0 MB (are excluded mysql, schema, etc...):"
 $mysql_cmd -N -s -r -e "SELECT table_schema 'Database Name', sum( data_length + index_length ) / 1024 / 1024 'Database Size (MB)' FROM information_schema.TABLES GROUP BY table_schema ;"|sed 's/\..*/MB/'|sed 's/\t/=/'|grep -v '=0MB'
