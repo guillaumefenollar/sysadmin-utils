5fad3fc7def7aec86a41a9d2da76956fb56bcbcf
Guillaume Fenollar <guillaume.fenollar@xwiki.com>
2012-10-01 11:18:37 +0200

    Adding script to count databases size on a mysql instance

---
+++ mysql-utils/detect_size_DB.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mysql_cmd='mysql -u root'
+
+echo "Here the size of databases, where size is above 0 MB (are excluded mysql, schema, etc...):"
+$mysql_cmd -N -s -r -e "SELECT table_schema 'Database Name', sum( data_length + index_length ) / 1024 / 1024 'Database Size (MB)' FROM information_schema.TABLES GROUP BY table_schema ;"|sed 's/\..*/MB/'|sed 's/\t/=/'|grep -v '=0MB'
